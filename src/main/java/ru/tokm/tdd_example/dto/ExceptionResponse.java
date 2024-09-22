78dbdc0e9d8baaebd3d32d45b1f419c12316f74f
Anton <antontokmakov757@gmail.com>
2024-09-22 12:33:58 +0700

    Add *.jar

--- src/main/java/ru/tokm/tdd_example/dto/ExceptionResponse.java
+++
@@ -1,4 +0,0 @@
-package ru.tokm.tdd_example.dto;
-
-public record ExceptionResponse(String statusName, int status, String message) {
-}
--- src/main/java/ru/tokm/tdd_example/service/CurrencyRateService.java
+++
@@ -1,9 +0,0 @@
-package ru.tokm.tdd_example.service;
-
-import ru.tokm.tdd_example.entity.CurrencyRate;
-
-public interface CurrencyRateService {
-
-    CurrencyRate getCurrencyRate(String url, String charCode);
-
-}
